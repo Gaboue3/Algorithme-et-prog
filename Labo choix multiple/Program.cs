6ca5c1b50503fa1759743a8aa2a00bb746aeedbd
Gaaaaaaaaaben <gabriel-ouellet-1999@hotmail.com>
2018-09-12 14:34:30 -0400

    Ajoutez des fichiers projet.

---
+++ Labo choix multiple/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Labo_choix_multiple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
